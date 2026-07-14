c991990adc51afad5ad9ba20c16bdce27874c602
Morg-Codes <Emazimorgan@outlook.com>
2026-07-13 23:25:20 -0300

    feat: add contact section and footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 "use strict";
 
 document.documentElement.classList.add("js");
+
+const currentYearElement = document.querySelector("#current-year");
+
+if (currentYearElement) {
+    currentYearElement.textContent = new Date().getFullYear();
+}
